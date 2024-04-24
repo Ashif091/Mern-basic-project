8b567bdab07d4c650f874eb6f7068c1970439c29
Ashif091 <fhyvhh091@gmail.com>
2024-04-25 03:57:46 +0530

    DB connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
-import experss from 'express'
+import experss from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log("Connected to MongoDB")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
 const app = experss()
 
-app.listen(3000,()=>{
-    console.log('Server running on prot 3000');
+app.listen(3000, () => {
+  console.log("Server running on prot 3000")
 })
